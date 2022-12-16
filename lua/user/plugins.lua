c9cf2c340f21252f3d976f611ed7fb2e386c0de8
王斌 <bingo084@outlook.com>
2022-12-16 14:45:13 +0800

    refactor: optimize packer config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,49 +1,33 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path,
-    }
-    print 'Installing packer close and reopen Neovim...'
-    vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+local packer_bootstrap = ensure_packer()
 
--- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
     vim.notify('packer is not found!')
     return
 end
 
--- Have packer use a popup window
-packer.init {
-    display = {
-        open_fn = function()
-            return require('packer.util').float { border = 'rounded' }
-        end,
-    },
-}
+vim.api.nvim_create_augroup("packer_user_config", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = "packer_user_config",
+    pattern = "plugins.lua",
+    command = "source <afile> | PackerSync"
+})
 
--- Install your plugins here
-return require('packer').startup(function(use)
-    -- My plugins here
+return packer.startup({ function(use)
     -- Manage packer itself
     use 'wbthomason/packer.nvim'
+    -- My plugins here
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {
@@ -107,8 +91,8 @@ return require('packer').startup(function(use)
     -- Git
     use 'lewis6991/gitsigns.nvim'
     -- Vim-kitty-navigater
-    use {'knubie/vim-kitty-navigator',
-        run= 'cp ./*.py ~/.config/kitty/'
+    use { 'knubie/vim-kitty-navigator',
+        run = 'cp ./*.py ~/.config/kitty/'
     }
     -- Color
     use 'norcalli/nvim-colorizer.lua'
@@ -117,4 +101,11 @@ return require('packer').startup(function(use)
     if packer_bootstrap then
         require('packer').sync()
     end
-end)
+end,
+    config = {
+        display = {
+            open_fn = function()
+                return require('packer.util').float({ border = 'rounded' })
+            end
+        }
+    } })
