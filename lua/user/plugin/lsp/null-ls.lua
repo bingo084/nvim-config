fe8da6ccc250192eeea3fc1430c7374e2d9c6cc8
王斌 <bingo084@outlook.com>
2022-12-23 17:56:31 +0800

    feat: change lua format to stylua

--- lua/user/plugin/lsp/null-ls.lua
+++ lua/user/plugin/lsp/null-ls.lua
@@ -13,13 +13,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
     debug = false,
     sources = {
-        --formatting.prettier.with {
-        --  extra_filetypes = { "toml", "solidity" },
-        --  extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-        --},
-        --formatting.black.with { extra_args = { "--fast" } },
-        --formatting.stylua,
+        formatting.stylua,
         formatting.google_java_format,
-        --diagnostics.flake8,
     },
 }
