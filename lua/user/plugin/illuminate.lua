dc4b765f86f51106a23a80bb41bf50193653b85d
王斌 <bingo084@outlook.com>
2023-01-04 09:04:43 +0800

    fix: remove illuminate in nvimtree

--- lua/user/plugin/illuminate.lua
+++ lua/user/plugin/illuminate.lua
@@ -2,6 +2,7 @@ require("illuminate").configure({
 	-- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
 	filetypes_denylist = {
 		"alpha",
+		"NvimTree",
 	},
 	-- modes_denylist: modes to not illuminate, this overrides modes_allowlist
 	-- See `:help mode()` for possible values
