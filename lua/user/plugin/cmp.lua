db3682167d194efc4d3d9092906fec5986ece952
王斌 <bingo084@outlook.com>
2023-01-09 08:42:42 +0800

    refactor: optimize nvim-cmp config

--- lua/user/plugin/cmp.lua
+++ lua/user/plugin/cmp.lua
@@ -10,16 +10,13 @@ end
 
 local compare = require("cmp.config.compare")
 
-require("luasnip/loaders/from_vscode").lazy_load()
-
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+local lspkind_status_ok, lspkind = pcall(require, "lspkind")
+if not lspkind_status_ok then
+	vim.notify("lspkind is not found!")
+	return
 end
 
 local icons = require("user.icons")
-
-local kind_icons = icons.kind
 
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
@@ -32,93 +29,65 @@ cmp.setup({
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
-
 	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-c>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		-- Accept currently selected item. If none selected, `select` first item.
-		-- Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Right>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.jumpable(1) then
-				luasnip.jump(1)
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif has_words_before() then
-				fallback()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+		["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+		["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+		["<Tab>"] = cmp.mapping(
+			cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+			{ "i", "c" }
+		),
+		["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+		["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+		["<C-u>"] = cmp.mapping(
+			cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select, count = 10 }),
+			{ "i", "c" }
+		),
+		["<C-d>"] = cmp.mapping(
+			cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select, count = 10 }),
+			{ "i", "c" }
+		),
 	}),
 	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			-- Kind icons
-			vim_item.kind = kind_icons[vim_item.kind]
-
-			if entry.source.name == "cmp_tabnine" then
-				vim_item.kind = icons.misc.Robot
-				vim_item.kind_hl_group = "CmpItemKindTabnine"
-			end
-
-			if entry.source.name == "copilot" then
-				vim_item.kind = icons.git.Octoface
-				vim_item.kind_hl_group = "CmpItemKindCopilot"
-			end
-
-			if entry.source.name == "emoji" then
-				vim_item.kind = icons.misc.Smiley
-				vim_item.kind_hl_group = "CmpItemKindEmoji"
-			end
-
-			-- NOTE: order matters
-			vim_item.menu = ({
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			menu = {
 				nvim_lsp = "[Lsp]",
 				nvim_lua = "[Lua]",
 				luasnip = "[Luasnip]",
 				buffer = "[Buffer]",
 				path = "[Path]",
 				emoji = "[Emoji]",
-			})[entry.source.name]
-			return vim_item
-		end,
+			},
+			symbol_map = { Copilot = icons.git.Octoface, Emoji = icons.misc.Smiley, Snippet = icons.kind.Snippet },
+			before = function(entry, vim_item)
+				if entry.source.name == "cmp_tabnine" then
+					vim_item.kind = icons.misc.Robot
+					vim_item.kind_hl_group = "CmpItemKindTabnine"
+				end
+
+				if entry.source.name == "copilot" then
+					vim_item.kind = icons.git.Octoface
+					vim_item.kind_hl_group = "CmpItemKindCopilot"
+				end
+
+				if entry.source.name == "emoji" then
+					vim_item.kind = icons.misc.Smiley
+					vim_item.kind_hl_group = "CmpItemKindEmoji"
+				end
+				return vim_item
+			end,
+		}),
 	},
 	sources = {
-		{ name = "nvim_lsp", group_index = 2 },
-		{ name = "nvim_lua", group_index = 2 },
-		{ name = "luasnip", group_index = 2 },
-		{ name = "buffer", group_index = 2 },
-		{ name = "path", group_index = 2 },
-		{ name = "emoji", group_index = 2 },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "emoji" },
 	},
 	sorting = {
 		priority_weight = 2,
@@ -131,7 +100,7 @@ cmp.setup({
 			compare.score,
 			compare.recently_used,
 			compare.locality,
-			-- compare.kind,
+			compare.kind,
 			compare.sort_text,
 			compare.length,
 			compare.order,
@@ -139,21 +108,20 @@ cmp.setup({
 			-- require("copilot_cmp.comparators").score,
 		},
 	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
+})
+
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
 	},
-	window = {
-		documentation = {
-			border = "rounded",
-			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-		},
-		completion = {
-			border = "rounded",
-			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-		},
-	},
-	experimental = {
-		ghost_text = true,
-	},
+})
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,12 +71,14 @@ return packer.startup({
 		use("hrsh7th/nvim-cmp") -- The completion plugin
 		use("hrsh7th/cmp-buffer") -- buffer completions
 		use("hrsh7th/cmp-path") -- path completions
+		use("hrsh7th/cmp-nvim-lua") -- path completions
 		use("hrsh7th/cmp-cmdline")
 		use("saadparwaiz1/cmp_luasnip")
 		use("hrsh7th/cmp-nvim-lsp")
 		use("hrsh7th/cmp-emoji")
+		use("onsails/lspkind.nvim")
 		-- snippets
-		use("L3MON4D3/LuaSnip") --snippet engine use 'hrsh7th/cmp-cmdline' -- cmdline completions
+		use("L3MON4D3/LuaSnip") --snippet engine 
 		-- Java
 		use("mfussenegger/nvim-jdtls")
 		-- Dap
