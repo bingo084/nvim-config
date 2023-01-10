c88fa80d70cec3a6a5312fe55e9da0000023b404
王斌 <bingo084@outlook.com>
2023-01-10 16:55:06 +0800

    fix: adjust vscode colorsheme config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
 local colorscheme = "vscode"
 
+vim.g.vscode_italic_comment = 1
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
