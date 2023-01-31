42555c303aa37c2170f9a67ad53b3680204503cd
王斌 <bingo084@outlook.com>
2023-01-31 09:47:02 +0800

    feat: markdown preview auto move chrome window next terminal

--- lua/bingo/plugins/markdown-preview.lua
+++ lua/bingo/plugins/markdown-preview.lua
@@ -6,8 +6,8 @@ return {
             function OpenMarkdownPreview (url)
                 execute "silent ! open -na 'Google Chrome' --args --new-window " . a:url
                 sleep 500m
-                " execute "silent ! yabai -m window  $(yabai -m query --windows --space 2 | jq '[.[].id]|max') --space 1"
-                " execute "silent ! yabai -m space --focus 1"
+                execute "silent ! yabai -m window  $(yabai -m query --windows --space 2 | jq '[.[].id]|max') --space 1"
+                execute "silent ! yabai -m space --focus 1"
                 execute "silent ! yabai -m window --focus prev"
             endfunction
             let g:mkdp_browserfunc = 'OpenMarkdownPreview'
