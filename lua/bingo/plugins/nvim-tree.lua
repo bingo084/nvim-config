71a40b52da8a04114d4bf79ce6bf913d565bc588
王斌 <bingo084@outlook.com>
2023-04-26 15:19:01 +0800

    fix(nvim-tree): migrating to on_attach

--- lua/bingo/plugins/nvim-tree.lua
+++ lua/bingo/plugins/nvim-tree.lua
@@ -32,16 +32,19 @@ return {
 			enable = true,
 			update_root = true,
 		},
-		view = {
-			mappings = {
-				list = {
-					{ key = { "l", "<CR>", "o" }, action = "edit" },
-					{ key = "h", action = "close_node" },
-					{ key = "<leader>-", action = "split" },
-					{ key = "<leader>\\", action = "vsplit" },
-				},
-			},
-		},
+		on_attach = function(bufnr)
+			local api = require("nvim-tree.api")
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+			api.config.mappings.default_on_attach(bufnr)
+			vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+			vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+			vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+			vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+			vim.keymap.set("n", "<leader>-", api.node.open.horizontal, opts("Open: Horizontal Split"))
+			vim.keymap.set("n", "<leader>\\", api.node.open.vertical, opts("Open: Vertical Split"))
+		end,
 		trash = {
 			cmd = "trash",
 		},
