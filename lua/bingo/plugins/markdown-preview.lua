b377eb3e8544898455ec6ba64681a4e653ac57b0
王斌 <bingo084@outlook.com>
2023-08-28 15:07:01 +0800

    refactor(markdown): remove custom OpenMarkDownPreview func

--- lua/bingo/plugins/markdown-preview.lua
+++ lua/bingo/plugins/markdown-preview.lua
@@ -1,18 +1,6 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		config = function()
-			vim.cmd([[
-            function OpenMarkdownPreview (url)
-                execute "silent ! open -na 'Google Chrome' --args --new-window " . a:url
-                sleep 500m
-                execute "silent ! yabai -m window  $(yabai -m query --windows --space 2 | jq '[.[].id]|max') --space 1"
-                execute "silent ! yabai -m space --focus 1"
-                execute "silent ! yabai -m window --focus prev"
-            endfunction
-            let g:mkdp_browserfunc = 'OpenMarkdownPreview'
-            ]])
-		end,
 		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
