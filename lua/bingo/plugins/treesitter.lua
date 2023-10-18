9a8a901b56946de7ef11f0becbc67af30acd4d8d
王斌 <bingo084@outlook.com>
2023-10-18 16:01:57 +0800

    feat(rainbow): migrate from nvim-ts-rainbow2 to rainbow-delimiters

--- lua/bingo/plugins/treesitter.lua
+++ lua/bingo/plugins/treesitter.lua
@@ -1,44 +1,66 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		"HiPhish/nvim-ts-rainbow2",
-		"windwp/nvim-ts-autotag",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"windwp/nvim-ts-autotag",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+				auto_install = true, -- Automatically install missing parsers when entering buffer
+				ignore_install = { "" }, -- List of parsers to ignore installing
+				highlight = {
+					-- use_languagetree = true,
+					enable = true, -- false will disable the whole extension
+					-- disable = { "css", "html" }, -- list of language that will be disabled
+					-- disable = { "css", "markdown" }, -- list of language that will be disabled
+					disable = { "markdown" }, -- list of language that will be disabled
+					-- additional_vim_regex_highlighting = true,
+				},
+				autopairs = {
+					enable = true,
+				},
+				indent = { enable = true, disable = { "python", "css", "rust", "java" } },
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+				autotag = {
+					enable = true,
+					disable = { "xml", "markdown" },
+				},
+			})
+		end,
+		event = "VeryLazy",
 	},
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-			auto_install = true, -- Automatically install missing parsers when entering buffer
-			ignore_install = { "" }, -- List of parsers to ignore installing
-			highlight = {
-				-- use_languagetree = true,
-				enable = true, -- false will disable the whole extension
-				-- disable = { "css", "html" }, -- list of language that will be disabled
-				-- disable = { "css", "markdown" }, -- list of language that will be disabled
-				disable = { "markdown" }, -- list of language that will be disabled
-				-- additional_vim_regex_highlighting = true,
-			},
-			autopairs = {
-				enable = true,
-			},
-			indent = { enable = true, disable = { "python", "css", "rust", "java" } },
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-			autotag = {
-				enable = true,
-				disable = { "xml", "markdown" },
-			},
-			rainbow = {
-				enable = true,
-				disable = { "html" },
-			},
-		})
-	end,
-	event = "VeryLazy",
-	keys = {
-		{ "<leader>Tr", "<cmd>TSToggle rainbow<cr>", desc = "Treesitter Rainbow" },
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			local rainbow_delimiters = require("rainbow-delimiters")
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>oR", "<cmd>lua require('rainbow-delimiters').toggle()<cr>", desc = "Toggle Rainbow" },
+		},
 	},
 }
