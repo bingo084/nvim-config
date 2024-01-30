6c86992d797df36a50efb9a62de19c16065f5cef
王斌 <bingo084@outlook.com>
2024-01-30 16:26:36 +0800

    fix(illuminate): opts in config not working

--- lua/bingo/plugins/illuminate.lua
+++ lua/bingo/plugins/illuminate.lua
@@ -1,15 +1,14 @@
 return {
 	"RRethy/vim-illuminate",
-	opts = {
-		filetypes_denylist = {
-			"alpha",
-			"NvimTree",
-		},
-		large_file_cutoff = 5000,
-		large_file_overrides = nil,
-	},
-	config = function(_, opts)
-		require("illuminate").configure({ opts })
+	config = function()
+		require("illuminate").configure({
+			filetypes_denylist = {
+				"alpha",
+				"NvimTree",
+			},
+			large_file_cutoff = 5000,
+			large_file_overrides = nil,
+		})
 	end,
 	event = "VeryLazy",
 	keys = {
