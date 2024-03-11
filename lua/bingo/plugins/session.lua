38387871415db84a6fde8a51951d14c134f1f6b8
王斌 <bingo084@outlook.com>
2024-03-11 22:28:41 +0800

    refactor(project,session): remove ahmedkhalf/project.nvim

--- lua/bingo/plugins/session.lua
+++ lua/bingo/plugins/session.lua
@@ -1,29 +1,27 @@
 return {
-	{
-		"olimorris/persisted.nvim",
-		lazy = false,
-		init = function()
-			vim.api.nvim_create_autocmd({ "User" }, {
-				pattern = "PersistedTelescopeLoadPre",
-				group = vim.api.nvim_create_augroup("PersistedHooks", {}),
-				callback = function()
-					require("persisted").save({ session = vim.g.persisted_loaded_session })
-					vim.cmd("wall")
-					vim.cmd("%bdelete!")
-				end,
-			})
-		end,
-		opts = {
-			silent = false,
-			use_git_branch = true,
-			autoload = true,
-			ignored_dirs = { { os.getenv("HOME"), exact = true } },
-		},
-		keys = {
-			{ "<leader>fs", "<cmd>Telescope persisted<cr>", desc = "[F]ind [S]ession" },
-			{ "<leader>sd", "<cmd>SessionDelete<CR>", desc = "[S]ession [D]elete" },
-			{ "<leader>sl", "<cmd>SessionLoad<CR>", desc = "[S]ession [L]oad" },
-			{ "<leader>ss", "<cmd>SessionSave<CR>", desc = "[S]ession [S]ave" },
-		},
+	"olimorris/persisted.nvim",
+	lazy = false,
+	init = function()
+		vim.api.nvim_create_autocmd({ "User" }, {
+			pattern = "PersistedTelescopeLoadPre",
+			group = vim.api.nvim_create_augroup("PersistedHooks", {}),
+			callback = function()
+				require("persisted").save({ session = vim.g.persisted_loaded_session })
+				vim.cmd("wall")
+				vim.cmd("%bdelete!")
+			end,
+		})
+	end,
+	opts = {
+		silent = false,
+		use_git_branch = true,
+		autoload = true,
+		ignored_dirs = { { os.getenv("HOME"), exact = true } },
+	},
+	keys = {
+		{ "<leader>fs", "<cmd>Telescope persisted<cr>", desc = "[F]ind [S]ession" },
+		{ "<leader>sd", "<cmd>SessionDelete<CR>", desc = "[S]ession [D]elete" },
+		{ "<leader>sl", "<cmd>SessionLoad<CR>", desc = "[S]ession [L]oad" },
+		{ "<leader>ss", "<cmd>SessionSave<CR>", desc = "[S]ession [S]ave" },
 	},
 }
--- lua/bingo/plugins/telescope-extension.lua
+++ lua/bingo/plugins/telescope-extension.lua
@@ -1,20 +1,5 @@
 return {
 	{
-		"ahmedkhalf/project.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("project_nvim").setup({
-				detection_methods = { "pattern" },
-			})
-			require("telescope").load_extension("projects")
-		end,
-		keys = {
-			{ "<leader>fp", "<cmd>Telescope projects<cr>", desc = "Find Projects" },
-		},
-	},
-	{
 		"ziontee113/icon-picker.nvim",
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
