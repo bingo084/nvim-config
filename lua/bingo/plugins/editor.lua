4cc7766e7fca4010d0a7ce282a009efbb204d24e
王斌 <bingo084@outlook.com>
2024-03-12 22:43:45 +0800

    feat(indent-blankline): add indent-blankline to editor.lua

--- lua/bingo/plugins/editor.lua
+++ lua/bingo/plugins/editor.lua
@@ -51,4 +51,32 @@ return {
 		event = "VeryLazy",
 		config = true,
 	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		dependencies = { "HiPhish/rainbow-delimiters.nvim" },
+		config = function()
+			require("ibl").setup({
+				indent = { char = "▏", tab_char = "▏" },
+				scope = {
+					highlight = {
+						"RainbowDelimiterRed",
+						"RainbowDelimiterYellow",
+						"RainbowDelimiterBlue",
+						"RainbowDelimiterOrange",
+						"RainbowDelimiterGreen",
+						"RainbowDelimiterViolet",
+						"RainbowDelimiterCyan",
+					},
+				},
+			})
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+			hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+			hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+		end,
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>oi", "<cmd>IBLToggle<cr>", desc = "Toggle Indent Blankline" },
+		},
+	},
 }
