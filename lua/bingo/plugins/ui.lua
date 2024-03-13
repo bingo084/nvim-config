9a82659c9cd84c791879f9624650c2aa7b89c965
王斌 <bingo084@outlook.com>
2024-03-13 23:32:32 +0800

    chore(noice): add some options to noice.nvim

--- lua/bingo/plugins/ui.lua
+++ lua/bingo/plugins/ui.lua
@@ -3,6 +3,13 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
 			presets = {
 				long_message_to_split = true,
 				inc_rename = true,
