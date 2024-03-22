1f5ee2e14a1044946f545ee4baba05c349d84aa5
王斌 <bingo084@outlook.com>
2024-03-22 14:59:17 +0800

    refactor(editor): simplify nvim-colorizer config

--- lua/bingo/plugins/editor.lua
+++ lua/bingo/plugins/editor.lua
@@ -11,12 +11,7 @@ return {
 	},
 	{
 		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup(
-				{ "*" },
-				{ RRGGBBAA = true, rgb_fn = true, hsl_fn = true, css = true, css_fn = true }
-			)
-		end,
+		config = function() require("colorizer").setup({ "*" }, { css = true }) end,
 		ft = { "css" },
 		keys = {
 			{ "<leader>oc", "<cmd>ColorizerToggle<cr>", desc = "Toggle Colorizer" },
