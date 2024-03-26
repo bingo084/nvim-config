7e8a8c56cf26fc69e3ed67ee636a5ae447392d33
王斌 <bingo084@outlook.com>
2024-03-26 15:56:57 +0800

    feat(telescope): set vim.ui.select to telescope

--- lua/bingo/plugins/telescope.lua
+++ lua/bingo/plugins/telescope.lua
@@ -27,6 +27,10 @@ return {
 				build = "make",
 				config = function() require("telescope").load_extension("fzf") end,
 			},
+			{
+				"nvim-telescope/telescope-ui-select.nvim",
+				config = function() require("telescope").load_extension("ui-select") end,
+			},
 		},
 		config = function()
 			require("telescope").setup({
