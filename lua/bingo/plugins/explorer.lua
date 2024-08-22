868257831dcf5e72f012e5d6f60c9580f5eb836e
王斌 <bingo084@outlook.com>
2024-08-22 11:46:05 +0800

    feat(yazi): lazy loading on keys or directory open

--- lua/bingo/plugins/explorer.lua
+++ lua/bingo/plugins/explorer.lua
@@ -35,6 +35,25 @@ return {
 	},
 	{
 		"mikavilpas/yazi.nvim",
+		init = function(plugin)
+			-- https://github.com/kevinm6/nvim/blob/0be7d3b1ece79ac5cca048ead5748681210defde/lua/plugins/editor/oil.lua#L150
+			if vim.fn.argc() == 1 then
+				local argv = tostring(vim.fn.argv(0))
+				local stat = (vim.uv or vim.loop).fs_stat(argv)
+				if stat and stat.type == "directory" then
+					require("lazy").load({ plugins = { plugin.name } })
+				end
+			end
+			if not require("lazy.core.config").plugins[plugin.name]._.loaded then
+				vim.api.nvim_create_autocmd("BufNew", {
+					pattern = "*/",
+					callback = function()
+						require("lazy").load({ plugins = { plugin.name } })
+						return true
+					end,
+				})
+			end
+		end,
 		---@module "yazi"
 		---@type YaziConfig
 		opts = {
@@ -45,7 +64,6 @@ return {
 				show_help = "~",
 			},
 		},
-		event = "VeryLazy",
 		keys = {
 			{ "<leader>e", "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
 			{ "<leader>E", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
