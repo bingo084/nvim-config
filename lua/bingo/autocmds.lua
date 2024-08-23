3d5497fe46a178216592f6edaa95b10f8f282dcf
王斌 <bingo084@outlook.com>
2024-08-23 17:45:52 +0800

    feat(autocmd): auto reload buffer if it's changed externally

--- lua/bingo/autocmds.lua
+++ lua/bingo/autocmds.lua
@@ -1,18 +1,26 @@
+local group = vim.api.nvim_create_augroup("custom", {})
+
 vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("disable-comment-auto-insertion", {}),
+	group = group,
 	callback = function() vim.opt.formatoptions:remove({ "r", "o" }) end,
 	desc = "Disable automatic comment insertion",
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight-on-yank", {}),
+	group = group,
 	callback = function() vim.highlight.on_yank() end,
 	desc = "Highlight when yanking (copying) text",
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*",
-	group = vim.api.nvim_create_augroup("chezmoi-apply-after-write", {}),
+	group = group,
 	callback = function() vim.cmd("!chezmoi apply --source-path %") end,
 	desc = "Chezmoi apply after write",
+})
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+	group = group,
+	callback = function() vim.cmd("checktime") end,
+	desc = "Reload buffer if it's changed externally",
 })
