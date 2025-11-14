6333bd43d8560915c495abf9ae4888b48069c2e8
bingo <bingo084@outlook.com>
2025-11-14 11:51:40 +0800

    feat(formatter): support optional dialect from first-line comment in sql_formatter

--- lua/bingo/plugins/formatter.lua
+++ lua/bingo/plugins/formatter.lua
@@ -17,6 +17,15 @@ return {
 				typescriptreact = { "prettierd" },
 				sql = { "sql_formatter" },
 			},
+			formatters = {
+				sql_formatter = {
+					prepend_args = function(_, ctx)
+						local first_line = vim.api.nvim_buf_get_lines(ctx.buf, 0, 1, false)[1] or ""
+						local language = first_line:match("%-%-%s*dialect%s*:%s*(%w+)")
+						return language and { "--language", language } or {}
+					end,
+				},
+			},
 			default_format_opts = {
 				lsp_format = "fallback",
 			},
