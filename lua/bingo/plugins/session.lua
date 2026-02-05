382e89dbc7c44b035accc6294d9ad3fea413fc13
bingo <bingo084@outlook.com>
2026-02-05 16:58:50 +0800

    fix(session): focus on none floating window when restore

--- lua/bingo/plugins/session.lua
+++ lua/bingo/plugins/session.lua
@@ -2,6 +2,25 @@ local function ps() return require("persisted") end
 return {
 	"olimorris/persisted.nvim",
 	lazy = false,
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			group = vim.api.nvim_create_augroup("PersistedLoadPreFocus", {}),
+			pattern = "PersistedLoadPre",
+			callback = function()
+				local function is_float(win_id)
+					return vim.wo[win_id].winfixwidth
+						or vim.wo[win_id].winfixheight
+						or vim.api.nvim_win_get_config(win_id).zindex
+				end
+				if is_float() then
+					vim.iter(vim.api.nvim_tabpage_list_wins(0))
+						:filter(function(win) return not (is_float(win)) end)
+						:take(1)
+						:each(function(win) vim.api.nvim_set_current_win(win) end)
+				end
+			end,
+		})
+	end,
 	opts = {
 		silent = false,
 		use_git_branch = true,
