caedd5e380261be4d1fe8803fee2215dcecf4a36
bingo <bingo084@outlook.com>
2026-02-11 18:47:18 +0800

    refactor(harpoon): rename explorer.lua to harpoon.lua

---
+++ lua/bingo/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+local function ui() return require("harpoon").ui end
+local function list() return require("harpoon"):list() end
+---@type LazySpec
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		local extensions = require("harpoon.extensions")
+		harpoon:setup({ settings = { save_on_toggle = true } })
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				local function menu_map(key, opt)
+					vim.keymap.set("n", key, function() ui():select_menu_item(opt) end, { buffer = cx.bufnr })
+				end
+				menu_map("<C-v>", { vsplit = true })
+				menu_map("<C-s>", { split = true })
+				menu_map("<C-t>", { tabedit = true })
+			end,
+		})
+		harpoon:extend(extensions.builtins.navigate_with_number())
+	end,
+	keys = {
+		{ "<C-e>", function() ui():toggle_quick_menu(list()) end, desc = "Harpoon Menu" },
+		{ "<C-m>", function() list():add() end, desc = "Harpoon Append" },
+		{ "<C-h>", function() list():select(1) end, desc = "Harpoon Select 1" },
+		{ "<C-j>", function() list():select(2) end, desc = "Harpoon Select 2" },
+		{ "<C-k>", function() list():select(3) end, desc = "Harpoon Select 3" },
+		{ "<C-l>", function() list():select(4) end, desc = "Harpoon Select 4" },
+		{ "<C-;>", function() list():prev() end, desc = "Harpoon Prev" },
+		{ "<C-'>", function() list():next() end, desc = "Harpoon next" },
+	},
+}
