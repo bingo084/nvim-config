c297c0574f1597c694917e270bad9c219dad95d9
bingo <bingo084@outlook.com>
2026-03-16 17:22:38 +0800

    feat(undo): add atone.nvim

--- lua/bingo/plugins/undo.lua
+++ lua/bingo/plugins/undo.lua
@@ -1,8 +1,17 @@
 ---@type LazySpec
 return {
-	"kevinhwang91/nvim-fundo",
-	dependencies = { "kevinhwang91/promise-async" },
-	build = function() require("fundo").install() end,
-	opts = {},
-	event = "BufReadPre",
+	{
+		"kevinhwang91/nvim-fundo",
+		dependencies = { "kevinhwang91/promise-async" },
+		build = function() require("fundo").install() end,
+		opts = {},
+		event = "BufReadPre",
+	},
+	{
+		"XXiaoA/atone.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>u", function() require("atone.core").toggle() end, desc = "Toggle Undo Tree" },
+		},
+	},
 }
